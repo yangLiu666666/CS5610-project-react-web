7c8f48abf7bf9113469de22afbe0615a26d7c6c9
liuyang <yang.liu2@northeastern.edu>
2022-12-08 22:02:11 -0500

    add add favorite button and remove favorite button

--- src/components/MealModal/index.js
+++ src/components/MealModal/index.js
@@ -2,19 +2,48 @@ import React, {useContext, useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import {MyContext} from "../../context";
-import axios from "axios";
+// import axios from "axios";
+import httpClient from "../../Axios";
 
 function MealModal({title, description, idMeal}) {
     const [show, setShow] = useState(false);
-    const {user} = useContext(MyContext)
-
+    const {user, setUser} = useContext(MyContext)
+    const [loading, setLoading] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const handleAddToFavorites = () => {
         console.log("mealId", idMeal);
-        axios.post('http://localhost:4000/add-favorites', {mealId: idMeal})
-            .then((res)=>console.log(res))
-            .catch((error) => console.log(error))
+        setLoading(true);
+        // axios.post('http://localhost:4000/add-favorites', {mealId: idMeal})
+        //     .then((res)=>console.log(res))
+        //     .catch((error) => console.log(error))
+        httpClient.post('/add-favorites', {mealId: idMeal})
+            .then(({data}) => {
+                setLoading(false);
+                setUser(data)
+                alert("Meal Added To Favorites")
+            })
+            .catch((error) => {
+                setLoading(false);
+                console.log(error)
+            })
+    }
+
+    const handleRemoveFromFavorites = () => {
+        setLoading(true);
+        // axios.post('http://localhost:4000/add-favorites', {mealId: idMeal})
+        //     .then((res)=>console.log(res))
+        //     .catch((error) => console.log(error))
+        httpClient.post('/remove-favorites', {mealId: idMeal})
+            .then(({data}) => {
+                setLoading(false);
+                setUser(data)
+                alert("Meal Removed From Favorites")
+            })
+            .catch((error) => {
+                setLoading(false);
+                console.log(error)
+            })
     }
     return (
         <>
@@ -31,11 +60,23 @@ function MealModal({title, description, idMeal}) {
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    {user &&
-                    <Button variant="primary" onClick={handleAddToFavorites}>
-                        Add To Favorites
-                    </Button>
-                    }
+                    {user && (
+                        <>
+                        {user.favorites.includes(idMeal) ? (
+                                <Button variant="danger"
+                                        onClick={handleRemoveFromFavorites}
+                                        disabled={loading}>
+                                    Remove From Favorites
+                                </Button>
+                            ) : (
+                                <Button variant="primary"
+                                        onClick={handleAddToFavorites}
+                                        disabled={loading}>
+                                    Add To Favorites
+                                </Button>)
+                        }
+                        </>
+                    )}
                 </Modal.Footer>
             </Modal>
         </>
