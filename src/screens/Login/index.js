3f6d4e386ca303a43cdbca573269b29c1e549428
liuyang <yang.liu2@northeastern.edu>
2022-12-09 11:20:56 -0500

    make login & register pretty

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -3,8 +3,10 @@ import Form from 'react-bootstrap/Form';
 import React, {useContext, useState} from "react";
 // import axios from "axios";
 import {MyContext} from "../../context";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import httpClient from "../../Axios";
+import {Col, Container, Row} from "react-bootstrap";
+import "./index.css"
 function Login() {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
@@ -32,35 +34,50 @@ function Login() {
             .catch((error)=>console.log(error));
     }
     return (
-        <Form onSubmit={handleLogin}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                    required
-                />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+        <Container>
+            <Row>
+                <Col md={7} className="d-flex align-items-center justify-content-center">
+                    <Form onSubmit={handleLogin} className="login-form">
+                        <h1 className="text-center">Login</h1>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                                required
+                            />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                        </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    required
-                />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Login
-            </Button>
-        </Form>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                required
+                            />
+                        </Form.Group>
+                        <div className="row mb-3 mt-4 justify-content-center">
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                        </div>
+                        <div className="py-4">
+                            <p className="text-center">
+                                Don't have an account?<Link to="/register">Register</Link>
+                            </p>
+                        </div>
+                    </Form>
+                </Col>
+                <Col md={5} className="login-bg-container"></Col>
+            </Row>
+        </Container>
     );
 }
 
--- src/screens/Register/index.js
+++ src/screens/Register/index.js
@@ -3,12 +3,14 @@ import Form from 'react-bootstrap/Form';
 import React, {useContext, useState} from 'react';
 // import axios from "axios";
 import {MyContext} from "../../context";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import httpClient from "../../Axios";
+import {Col, Container, Row} from "react-bootstrap";
 function Register() {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [role, setRole] = useState('');
     const {setUser} = useContext(MyContext);
     function handleRegister(e) {
         e.preventDefault();
@@ -25,36 +27,58 @@ function Register() {
             .catch((error)=>console.log(error));
     }
     return (
-        <Form onSubmit={handleRegister}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                    required
-                />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+        <Container>
+            <Row>
+                <Col md={7} className="d-flex align-items-center justify-content-center">
+                    <Form onSubmit={handleRegister} className="login-form">
+                        <h1 className="text-center">Register</h1>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                                required
+                            />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    required
-                />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                required
+                            />
+                        </Form.Group>
+                        <div>
+                            <div className="row justify-content-center">
+                                <label>
+                                    Role:
+                                    <select defaultValue={"user"} className="form-select form-select-sm m-1" aria-label="Default select example"
+                                            onChange={(e) => setRole(e.target.value)}>
+                                        <option value="user">User</option>
+                                        <option value="premium">Premium</option>
+                                        <option value="admin">Admin</option>
+                                    </select>
+                                </label>
+                            </div>
+                        </div>
+                        <div className="row mb-3 mt-4 justify-content-center">
+                        <Button variant="primary" type="submit">
+                            Register
+                        </Button>
+                        </div>
+                    </Form>
+                </Col>
+                <Col md={5} className="login-bg-container"></Col>
+            </Row>
+        </Container>
     );
 }
-
 export default Register;
